1b905975c7c17dfc101763ec6bab56849470cf7e
parkikbum <pibum@naver.com>
2022-01-08 15:02:10 +0900

    [ADD]컬러 익스텐션 추가

--- SeeMeet/SeeMeet/Source/Extension/UIColor++.swift
+++ SeeMeet/SeeMeet/Source/Extension/UIColor++.swift
@@ -5,6 +5,38 @@
 //  Created by 박익범 on 2022/01/05.
 //
 
-import Foundation
+import UIKit
 
-//색상표 디자이너분들이 모아주시면 업데이트
+extension UIColor {
+    @nonobjc class var white: UIColor {
+        return UIColor(white: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var pink01: UIColor {
+        return UIColor(red: 250.0 / 255.0, green: 85 / 255.0, blue: 92 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var pink02: UIColor {
+        return UIColor(red: 254.0 / 255.0, green: 221.0 / 255.0, blue: 222.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grey01: UIColor {
+        return UIColor(red: 246.0 / 255.0, green: 246.0 / 255.0, blue: 246.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grey02: UIColor {
+        return UIColor(red: 231.0 / 255.0, green: 231.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grey03: UIColor {
+        return UIColor(red: 163.0 / 255.0, green: 163.0 / 255.0, blue: 163.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grey04: UIColor {
+        return UIColor(red: 75.0 / 255.0, green: 75.0 / 255.0, blue: 75.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grey05: UIColor {
+        return UIColor(red: 75.0 / 255.0, green: 75.0 / 255.0, blue: 75.0 / 255.0, alpha: 1.0)
+    }
+}
