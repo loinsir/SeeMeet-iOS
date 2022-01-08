9365f69e2764abb6f95ef227b82d799979bf549f
parkikbum <pibum@naver.com>
2022-01-08 19:58:00 +0900

    [ADD] 홈뷰 다가오는 약속 셀 구현

--- SeeMeet/SeeMeet/Source/Views/Cells/CVC/HomeScene/HomeEventCVC.swift
+++ SeeMeet/SeeMeet/Source/Views/Cells/CVC/HomeScene/HomeEventCVC.swift
@@ -14,14 +14,13 @@ class HomeEventCVC: UICollectionViewCell {
     static let identifier: String = "HomeEventCVC"
     
 //MARK: Componnents
-
     private let dDayView = UIView().then{
         $0.backgroundColor = UIColor.pink01
         $0.clipsToBounds = true
-        $0.layer.cornerRadius = 18
+        $0.layer.cornerRadius = 14
     }
     private let dDayLabel = UILabel().then{
-        $0.font = UIFont.dinProBoldFont(ofSize: 24)
+        $0.font = UIFont.dinProBoldFont(ofSize: 14)
         $0.textColor = UIColor.white
         $0.textAlignment = .center
     }
@@ -40,17 +39,17 @@ class HomeEventCVC: UICollectionViewCell {
     }
 //MARK: Layout
     func setBackgroundViewLayout() {
-        self.getShadowView(color: UIColor.black.withAlphaComponent(0.25).cgColor, masksToBounds: true, shadowOffset: CGSize(width: 2, height: 4), shadowRadius: 10, shadowOpacity: 0.25)
-        self.backgroundColor = UIColor.white
-        self.clipsToBounds = true
-        self.layer.cornerRadius = 18
+        getShadowView(color: UIColor.black.cgColor, masksToBounds: false, shadowOffset: CGSize(width: 0, height: 4), shadowRadius: 3, shadowOpacity: 0.25)
+        contentView.clipsToBounds = true
+        contentView.backgroundColor = UIColor.white
+        contentView.layer.cornerRadius = 18
     }
     func setLayout() {
         addSubviews([dDayView, eventImageView, eventNameLabel, eventDateLabel])
         dDayView.addSubview(dDayLabel)
         
         dDayView.snp.makeConstraints{
-            $0.top.equalTo(11)
+            $0.top.equalToSuperview().offset(11)
             $0.width.equalTo(53)
             $0.height.equalTo(29)
             $0.centerX.equalToSuperview()
@@ -61,34 +60,33 @@ class HomeEventCVC: UICollectionViewCell {
             $0.centerY.equalToSuperview()
         }
         eventImageView.snp.makeConstraints{
-            $0.top.equalTo(9)
+            $0.top.equalTo(dDayView.snp.bottom).offset(9)
             $0.width.height.equalTo(82)
             $0.centerX.equalToSuperview()
         }
         eventNameLabel.snp.makeConstraints{
             $0.top.equalTo(eventImageView.snp.bottom).offset(16)
-            $0.leading.trailing.equalToSuperview().offset(19)
+            $0.leading.equalToSuperview().offset(19)
+            $0.trailing.equalToSuperview().offset(-19)
             $0.height.equalTo(18)
         }
         eventDateLabel.snp.makeConstraints{
-            $0.top.equalTo(eventDateLabel.snp.bottom).offset(8)
-            $0.leading.trailing.equalTo(52)
+            $0.top.equalTo(eventNameLabel.snp.bottom).offset(8)
+            $0.leading.equalToSuperview().offset(10)
+            $0.trailing.equalToSuperview().offset(-10)
             $0.height.equalTo(18)
         }
         
     }
     
 //MARK: Function
-    
     func setData(dDay: String, image: String, eventName: String, eventData: String){
         dDayLabel.text = dDay
         eventImageView.image = UIImage(named: image)
         eventNameLabel.text = eventName
         eventDateLabel.text = eventData
     }
-    
-    
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         setBackgroundViewLayout()
