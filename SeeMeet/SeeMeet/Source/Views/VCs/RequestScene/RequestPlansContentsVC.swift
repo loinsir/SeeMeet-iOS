30607a65b6a3c516b7317d7e0da9afdba9886e44
Yujin Lee <yj587878@gmail.com>
2022-01-14 04:04:29 +0900

    [IMPROVE] 친구 검색창 TextField에서 SearchBar로 변경

--- SeeMeet/SeeMeet/Source/Views/VCs/RequestScene/RequestPlansContentsVC.swift
+++ SeeMeet/SeeMeet/Source/Views/VCs/RequestScene/RequestPlansContentsVC.swift
@@ -25,6 +25,7 @@ class RequestPlansContentsVC: UIViewController {
     private let friendSelectionLabel = UILabel().then{
         $0.text = "약속 신청할 친구를 선택하세요"
         $0.font = UIFont.hanSansRegularFont(ofSize: 18)
+        $0.textColor = UIColor.grey06
         let attributedString = NSMutableAttributedString(string: "약속 신청할 친구를 선택하세요")
         attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.pink01, range: ($0.text! as NSString).range(of:"친구"))
         attributedString.addAttribute(.font, value: UIFont.hanSansBoldFont(ofSize: 18), range: ($0.text! as NSString).range(of: "친구"))
@@ -32,7 +33,7 @@ class RequestPlansContentsVC: UIViewController {
         
 
     }
-    
+    /*
     private let searchTextField = UITextField().then{
         $0.backgroundColor = UIColor.grey01
         $0.layer.cornerRadius = 10
@@ -41,15 +42,42 @@ class RequestPlansContentsVC: UIViewController {
         $0.addLeftPadding(width: 46)
         
     }
+     */
     
+    private let searchBar = UISearchBar().then{
+
+        $0.placeholder = "받는 사람:"
+        $0.setImage(UIImage(named: "ic_search"), for: UISearchBar.Icon.search, state: .normal)
+        $0.backgroundColor = UIColor.grey01
+        $0.backgroundImage = UIImage()
+        $0.searchBarStyle = .minimal
+        $0.isTranslucent = false
+        $0.layer.cornerRadius = 10
+        if let searchBarStyle = $0.value(forKey: "searchField") as? UITextField{
+            searchBarStyle.clearButtonMode = .never
+        }
+       
+        if let textfield = $0.value(forKey: "searchField") as? UITextField {
+            textfield.attributedPlaceholder = NSAttributedString(string: textfield.placeholder ?? "", attributes: [.foregroundColor: UIColor.grey04, .font: UIFont.hanSansRegularFont(ofSize: 14)])
+            textfield.textColor = UIColor.grey06
+            textfield.layer.cornerRadius = 10
+            textfield.font = UIFont.hanSansRegularFont(ofSize: 14)
+ 
+        }
+        
+    }
+    
+   /*
     private let searchImageView = UIImageView().then{
         $0.image = UIImage(named: "ic_search")
         
     }
+    */
     
     private let contentsWritingLabel = UILabel().then{
         $0.text = "약속의 내용을 작성하세요"
         $0.font = UIFont.hanSansRegularFont(ofSize: 18)
+        $0.textColor = UIColor.grey06
         let attributedString = NSMutableAttributedString(string: "약속의 내용을 작성하세요")
         attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.pink01, range: ($0.text! as NSString).range(of:"내용"))
         attributedString.addAttribute(.font, value: UIFont.hanSansBoldFont(ofSize: 18), range: ($0.text! as NSString).range(of: "내용"))
@@ -63,6 +91,7 @@ class RequestPlansContentsVC: UIViewController {
     
     private let plansTitleTextField = UITextField().then{
         $0.font = UIFont.hanSansBoldFont(ofSize: 14)
+        $0.textColor = UIColor.grey06
         $0.attributedPlaceholder = NSAttributedString(string: "제목", attributes: [.foregroundColor: UIColor.grey04, .font: UIFont.hanSansRegularFont(ofSize: 14)])
     }
     
@@ -75,6 +104,7 @@ class RequestPlansContentsVC: UIViewController {
     private let plansContentsTextView = UITextView().then{
         $0.font = UIFont.hanSansRegularFont(ofSize: 14)
         $0.backgroundColor = UIColor.grey01
+        $0.textColor = UIColor.grey06
         $0.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         $0.textContainer.lineFragmentPadding = 0
         $0.scrollIndicatorInsets = $0.textContainerInset
@@ -107,9 +137,9 @@ class RequestPlansContentsVC: UIViewController {
 //MARK: Layout
     func setLayout() {
         self.navigationController?.isNavigationBarHidden = true
-        view.addSubviews([titleView,friendSelectionLabel,searchTextField,contentsWritingLabel,plansContentsView,navigationLineView,nextButton])
+        view.addSubviews([titleView,friendSelectionLabel,searchBar,contentsWritingLabel,plansContentsView,navigationLineView,nextButton])
         titleView.addSubviews([titleLabel,closeButton])
-        searchTextField.addSubview(searchImageView)
+
         plansContentsView.addSubviews([plansTitleTextField,seperateLineView,plansContentsTextView])
         
         titleView.snp.makeConstraints{
@@ -130,19 +160,26 @@ class RequestPlansContentsVC: UIViewController {
             $0.leading.equalToSuperview().offset(20)
             $0.height.equalTo(32)
         }
-        searchTextField.snp.makeConstraints{
+        searchBar.snp.makeConstraints{
             $0.top.equalTo(friendSelectionLabel.snp.bottom).offset(7)
             $0.leading.equalTo(friendSelectionLabel)
             $0.trailing.equalToSuperview().offset(-20)
             $0.height.equalTo(50)
         }
+    
+        searchBar.searchTextField.snp.makeConstraints{
+            $0.top.leading.bottom.trailing.equalToSuperview()
+        }
+        
+        /*
         searchImageView.snp.makeConstraints{
             $0.centerY.equalToSuperview()
             $0.leading.equalToSuperview().offset(8)
             $0.width.height.equalTo(34)
         }
+         */
         contentsWritingLabel.snp.makeConstraints{
-            $0.top.equalTo(searchTextField.snp.bottom).offset(38)
+            $0.top.equalTo(searchBar.snp.bottom).offset(38)
             $0.leading.equalToSuperview().offset(20)
             $0.height.equalTo(32)
         }
@@ -187,7 +224,7 @@ class RequestPlansContentsVC: UIViewController {
     
 //MARK: Delegate
     func setDelegate(){
-        searchTextField.delegate = self
+        searchBar.delegate = self
         plansTitleTextField.delegate = self
         plansContentsTextView.delegate = self
     }
@@ -205,7 +242,11 @@ extension RequestPlansContentsVC: UITextViewDelegate{
         if textView.textColor == UIColor.grey04{
             textView.text = nil
             textView.textColor = UIColor.black
-        }
+        }/*
+        if (!textView.text.isEmpty) && (textView.text != "약속 상세 내용") {
+            textView.text = nil
+            textView.textColor = UIColor.black
+          */
         plansContentsView.layer.borderColor = UIColor.pink01.cgColor
         plansContentsView.layer.borderWidth = 1.0
     }
@@ -232,10 +273,11 @@ extension RequestPlansContentsVC: UITextViewDelegate{
 
 extension RequestPlansContentsVC: UITextFieldDelegate{
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        switch textField{
+        switch textField{/*
         case searchTextField:
             textField.layer.borderColor = UIColor.pink01.cgColor
             textField.layer.borderWidth = 1.0
+                          */
         case plansTitleTextField:
             plansContentsView.layer.borderColor = UIColor.pink01.cgColor
             plansContentsView.layer.borderWidth = 1.0
@@ -247,8 +289,10 @@ extension RequestPlansContentsVC: UITextFieldDelegate{
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         switch textField{
+            /*
         case searchTextField:
             textField.layer.borderWidth = 0
+             */
         case plansTitleTextField:
             plansContentsView.layer.borderWidth = 0
         default:
@@ -257,3 +301,14 @@ extension RequestPlansContentsVC: UITextFieldDelegate{
     }
 }
 
+extension RequestPlansContentsVC: UISearchBarDelegate{
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.layer.borderColor = UIColor.pink01.cgColor
+        searchBar.layer.borderWidth = 1.0
+       
+    }
+
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.layer.borderWidth = 0
+    }
+}
