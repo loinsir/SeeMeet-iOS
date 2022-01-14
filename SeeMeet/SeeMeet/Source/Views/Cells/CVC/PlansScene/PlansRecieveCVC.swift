22701230231fb29fdb3ae76a25469f4ee22757a0
parkikbum <pibum@naver.com>
2022-01-15 03:01:06 +0900

    [ADD] 받은 약속 뷰 셀 구현완료

---
+++ SeeMeet/SeeMeet/Source/Views/Cells/CVC/PlansScene/PlansRecieveCVC.swift
@@ -0,0 +1,17 @@
+//
+//  PlansRecieveCVC.swift
+//  SeeMeet
+//
+//  Created by 박익범 on 2022/01/15.
+//
+
+import UIKit
+
+class PlansRecieveCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
