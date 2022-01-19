25286e22ae0c52f05223ae719469cc192800ba8f
parkikbum <pibum@naver.com>
2022-01-20 01:52:46 +0900

    [ADD] 데이터 리스트 뷰 유알엘 추가

---
+++ SeeMeet/SeeMeet/Source/Models/PlansListDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlansListDataModel.swift
+//  SeeMeet
+//
+//  Created by 박익범 on 2022/01/19.
+//
+
+import Foundation
---
+++ SeeMeet/SeeMeet/Source/Services/GetPlansListDataService.swift
@@ -0,0 +1,8 @@
+//
+//  GetPlansListDataService.swift
+//  SeeMeet
+//
+//  Created by 박익범 on 2022/01/19.
+//
+
+import Foundation
